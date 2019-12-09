4c87879a87ee2e70d2c1d2cc82504f7c6de866cf
Mohammed A. Shaawa <mshaawa963@gmail.com>
2019-12-09 22:05:16 +0300

    Changing `DEBUG` to False

--- arabcodingcommunity/settings.py
+++ arabcodingcommunity/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '#beotfi)t)zzsfekd)jixqfafj0on17)&p@siz_lt$g^d75tjj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
